90ee2568482862e1ef777ad1f4f44402c1b67296
Ademsk1 <adam.illoul24@gmail.com>
2023-01-09 17:10:28 +0000

    Added navigation bar component

---
+++ stylestudio/src/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import {Link} from 'react-router-dom'
+export function NavigationBar() {
+    return (
+        <div>
+        <Navbar bg='dark' variant='dark'>
+
+            <Navbar.Brand className='ml-2'>StyleStudio</Navbar.Brand>
+            <Nav className='me-auto'>
+                <Nav.Link ><Link to='/home'>Home</Link></Nav.Link>
+                <Nav.Link>Specificity Leaderboard</Nav.Link>
+                <Nav.Link href='#forums'>Forums</Nav.Link>
+                <Nav.Link href='#guides'>Guides</Nav.Link>
+                <Nav.Link><Link to='/login'>Login</Link></Nav.Link>
+            </Nav>
+        </Navbar>
+        </div>
+    )
+
+}
+
+
+
+
