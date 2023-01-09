bb33379ae125dd1c3c8b518c3ba9db02743c847c
Ademsk1 <adam.illoul24@gmail.com>
2023-01-09 17:11:26 +0000

    adding in react-router-dom dependencies

--- stylestudio/src/App.js
+++ stylestudio/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {NavigationBar} from './components/NavigationBar.js'
+import {Route, Routes} from 'react-router-dom'
+import { HomePage } from './pages/HomePage/HomePage.js';
+import {LoginPage} from './pages/LoginPage/LoginPage.js'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar/>
+      <Routes>
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
     </div>
   );
 }
