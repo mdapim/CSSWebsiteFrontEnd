ba686cf7a2c1bbf6560957691a3a2bfe77caad8c
Ademsk1 <adam.illoul24@gmail.com>
2023-01-10 13:24:07 +0000

    specificity calculator is now able to send a sorted list of css attributes based on specificity

---
+++ stylestudio/src/Utilities/SpecificityCalculator.js
@@ -0,0 +1,51 @@
+const { calculate } = require('specificity');
+
+
+const cssInput = `ul#nav li.active a {
+    background-color:green;
+}
+
+body {
+    width:auto;
+}
+
+div p {
+    color:green;
+}`
+
+function findCSSCommands(cssInput) {
+    const seperatedCSS = cssInput.split(/[{}\n]/g)
+    const filteredCSS = seperatedCSS.filter(el=>el!=='')
+    const cssObject = {}
+    let storedKey=''
+    for (let i =0;i<filteredCSS.length;i++) {
+        const line = filteredCSS[i]
+        if (!line.startsWith('    ')) {
+            cssObject[line]=[]
+            storedKey = line
+        }
+        else {
+            cssObject[storedKey].push(line.replace('    ',''))
+        }
+    }
+    return cssObject
+}
+function findSpecificity(cssObject) {
+    const cssSpecObj = {}
+    const cssSpecArr = []
+    let i=0
+    for (let key of Object.keys(cssObject)) {
+        const cssSpec = calculate(key)[0].specificityArray
+        cssSpecArr.push(cssSpec)
+        cssSpecObj[key]=cssSpec
+    }
+    const sortedSpec = cssSpecArr.sort().reverse()
+    const order = sortedSpec.map(spec=> {
+        return Object.keys(cssSpecObj).filter(el=>cssSpecObj[el]===spec)
+    })
+    console.log(order)
+
+}
+
+const cssObject = findCSSCommands(cssInput)
+findSpecificity(cssObject)
