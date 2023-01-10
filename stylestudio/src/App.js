5a95b03aaf0e00b57c79ee7f2f811af32982f905
Ademsk1 <adam.illoul24@gmail.com>
2023-01-10 10:33:09 +0000

    added login, signup, homepage basics, the two former being able to communicate with the backend successfully

--- stylestudio/src/App.js
+++ stylestudio/src/App.js
@@ -2,16 +2,28 @@ import {NavigationBar} from './components/NavigationBar.js'
 import {Route, Routes} from 'react-router-dom'
 import { HomePage } from './pages/HomePage/HomePage.js';
 import {LoginPage} from './pages/LoginPage/LoginPage.js'
+import {Signup} from './pages/Signup/Signup.js'
+import {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+  const [signInCredentials,setSignInCredentials] = useState({name:'',password:''})
+  const handleSignInChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setSignInCredentials((prev) => {
+      return { ...prev, [name]: value };
+    });
+    console.log(signInCredentials);
+  };
   return (
     <div className="App">
       <NavigationBar/>
       <Routes>
         <Route path="/home" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/login" element={<LoginPage handleSignInChange={handleSignInChange} signInCredentials={signInCredentials}/>}/>
+        <Route path='/sign-up' element={<Signup handleSignInChange={handleSignInChange} signInCredentials={signInCredentials}/>}/>
       </Routes>
     </div>
   );
