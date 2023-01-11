06c5b845c66979458677b34d74911090931c6f74
Ademsk1 <adam.illoul24@gmail.com>
2023-01-11 17:53:19 +0000

    figuring out git issues:

--- stylestudio/src/pages/Guides/Resource.js
+++ stylestudio/src/pages/Guides/Resource.js
@@ -1,15 +1,11 @@
 export function Resource({title,links}) {
-    
-
     const mapLinks=resources=> {
         const resourceJSX = resources.map(resource=> {
             return <li><a href={resource.resource_link}>{resource.resource_description}</a></li>
         })
         return resourceJSX
     }
-
     return (
-        
         <div className='content'>
         <h3>
             {title}
