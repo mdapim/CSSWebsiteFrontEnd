0bb7acd505573193d59eda3adb8a5af0a5f8f00e
Callum Hall <callumhall@Callums-MacBook-Pro.local>
2023-01-13 15:32:19 +0000

    fixing bug with sphere

--- stylestudio/src/pages/HomePage/HomePage.js
+++ stylestudio/src/pages/HomePage/HomePage.js
@@ -1,10 +1,47 @@
-import TextShpere from "./components/Sphere.js";
-import React from "react";
+import TagCloud from "TagCloud";
+import React, { useEffect } from "react";
+import "./Sphere.css";
 
 export function HomePage() {
+  useEffect(() => {
+    return () => {
+      const container = ".tagcloud";
+      const texts = [
+        "CSS",
+        "Animations",
+        "JavaScript",
+        "CSS3",
+        "Blur",
+        "Shadow",
+        "Flex",
+        "Rolling",
+        "Sphere",
+        "6KB",
+        "v2.x",
+        "Sphere",
+        "6KB",
+        "v2.x",
+        "Sphere",
+        "6KB",
+        "adam",
+        "mike",
+        "cal",
+      ];
+      const options = {
+        radius: 300,
+        maxSpeed: "fast",
+        initSpeed: "fast",
+        keep: true,
+      };
+      TagCloud(container, texts, options);
+    };
+  }, []);
+
   return (
     <div>
-      <TextShpere />
+      <div className="text-shpere">
+        <span className="tagcloud"></span>
+      </div>
     </div>
   );
 }
